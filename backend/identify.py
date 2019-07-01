b6aad77055112f698f3ee94b0a6dc5eab62b6071
Xdecosee <finalfantasy_1998@hotmail.com>
2019-07-01 12:51:08 +0800

    change to module style

--- backend/identify.py
+++ backend/identify.py
@@ -1,36 +1,17 @@
-#!/usr/bin/python3
-
 '''
 AUTHOR: Annette
-PROGRAM DESC: ARP Ping for live devices with Scapy
+PROGRAM DESC: Identify Module for device discovery
 
 TODO (Add on):
 Feature 1 - Return value for Empty Set values
-
 Feature 2 - Decide on background thread for device identification
-- If don't want background thread:
-1. app start 
-2. user click "scan devices" 
-3. get_subnets() 
-4. scan_devices() 
-5. mac_lookup() on each live device => decide on display + saving to db
-6. create sniff() function thread with filter for each live device 
-to do live traffic capture
-
-- If converting to a background thread:
-1. app start
-2. user click "monitor on"
-3. get_subnets() 
-4. Background Thread to use 'arping' continuously (send() or sendp() or srploop())
-5. mac_lookup() on each live device => decide on display + saving to db
-5. Create sniff() function tread with filter for each live device to do live
-traffic capture
+Feature 3 - Class files for devices and packets to access values more easily
 '''
-
 
 from netaddr import IPAddress, IPNetwork
 from scapy.all import arping, conf
 import requests
+
 
 '''
 Return: Gateway ip address from 0.0.0.0 network address 
@@ -41,7 +22,7 @@ E.g.
 Network          Netmask          Gateway        
 0.0.0.0          0.0.0.0          192.168.1.254
 '''
-def get_gateway_ip():
+def get_gateway():
         #Ref: Scapy Routing Table
         conf.route.resync()
         routes = conf.route.routes
@@ -49,13 +30,15 @@ def get_gateway_ip():
                 if route[0] == 0 and route[1] == 0:
                         return route[2]
 
+
 '''
-Return: List of subnets (CIDR) from routing table where gateway ip is in
+Input: Gateway Ip Address
+Return: Set of subnets (CIDR) from routing table where gateway ip is in
 '''
 def get_subnets():
         
         #Ref: IOT Inspector utils.py
-        gateway_ip = get_gateway_ip()
+        gateway_ip = get_gateway()
         subnet_set = set()
 
         if gateway_ip:
@@ -76,19 +59,7 @@ def get_subnets():
                                 subnet_set.add(subnet)
                                 return subnet_set
 
-'''
-Input: List of subnets (CIDR) from routing table where gateway ip is in 
-Return: List of live hosts found within each subnet after arp ping
-'''
-def scan_devices(subnet_set):
 
-        ans_lists = set()
-
-        for subnet in subnet_set:
-                ans,unans = arping(str(subnet))
-                ans_lists.add(ans)
-                
-        return ans_lists
 
 '''
 Input: Device Mac Address
@@ -100,26 +71,29 @@ def mac_lookup(mac):
         result = requests.get(MAC_URL % mac).json()
         return result['result']['company']
 
+
 '''
-Input: List of live hosts found within each subnet after arp ping
-Output: List of devices (IP Address, Mac Address, Vendor)
+Input: Set of subnets (CIDR) from routing table where gateway ip is in 
+Return: Set of live hosts & their vendor (tuple) found within each subnet after arp ping
 '''
-def save_devices(ans_lists):
+def scan_devices(subnet_set):
+
+        ans_lists = set()
+        dev_list = set()
+
+        for subnet in subnet_set:
+                ans,unans = arping(str(subnet))
+                ans_lists.add(ans)
 
         for ans in ans_lists:
                 for dev in ans: 
-                        
                         company = mac_lookup(dev[1].src)
-                        print(dev[1].psrc, dev[1].src, company)
+                        new_dev = (dev[1].psrc, dev[1].src, company)
+                        dev_list.add(new_dev)
+                
+        return dev_list
 
 
-if __name__== "__main__" : 
-      
-        #Get list of subnets gateway is in (CIDR)
-        subnet_set = get_subnets()
-        #Scan for Live Hosts
-        ans_lists = scan_devices(subnet_set)
-        #Func: Lookup Vendor name + Update users' database
-        save_devices(ans_lists)
 
+    
 
