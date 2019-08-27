1bb403b9e6d87e629344e12b00e02d53f12a7776
Kezia <keziakew98@gmail.com>
2019-08-28 06:11:39 +0800

    update

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ except: print " > Database fail"
 
 @app.route('/') #Logout redirect page
 def startup():
-    print ' > Launching login'
+    print ' > Launching startup'
     return render_template("login.html")
 
 @app.route('/login', methods=['POST'])
@@ -85,25 +85,45 @@ def login():
 
         if (password==retrievedPwd):
             print " > User matched"
-            return render_template("devices.html")
+            return render_template("start.html")
         
         else:
             return render_template("login.html")
-
-@app.route('/devices')
-def devices():
-    print ' > Launching devices'
-    return render_template("devices.html")
 
 @app.route('/start')
 def start():
     print ' > Launching start'
     return render_template("start.html")
 
+@app.route('/devices')
+def devices():
+    print ' > Launching devices'
+
+    #cursor.execute("SELECT DISTINCT deviceName from Devices")
+
+    return render_template("devices.html")
+    #return render_template("devices.html", my_list=range(10))
+
+
 @app.route('/deviceDetails')
 def deviceDetails():
     print ' > Launching devices details'
     return render_template("deviceDetails.html")
+
+@app.route('/update', methods=['POST'])
+def update():
+    print ' > Updating device name'
+
+    details = request.form
+    newName = details['deviceName']
+    
+    cursor.execute("SELECT deviceName FROM Traffic WHERE (deviceName LIKE \"%s\")" % newName )
+    dbResult = cursor.fetchall()
+
+    #oldName = temp TODO
+    
+    cursor.execute("""UPDATE Traffic SET deviceName=%s WHERE deviceName = %s""", ( newName, oldName ))
+    conn.commit()
 
 @app.route('/reset')
 def reset():
